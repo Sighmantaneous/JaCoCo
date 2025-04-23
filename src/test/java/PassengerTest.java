367616c8c347440b18c3ce35c62997016fb4f71a
sighman <g00286398@atu.ie>
2025-04-23 12:13:11 +0100

    Competed Tests for pass and fail for title

--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -1,4 +1,7 @@
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PassengerTest {
@@ -17,6 +20,34 @@ public class PassengerTest {
         assertEquals("Todd", myPass.getFname());
         assertEquals("Umptious", myPass.getLname());
 
+    }
+
+
+    //Optional way to test
+    /*
+    @Test
+    void titleTestSuccess(){
+        assertEquals("Mr",myPass.getTitle());
+        Passenger myPass2 = new Passenger("Mrs","Mary","O'Leary");
+        assertEquals("Mrs",myPass2.getTitle());
+        Passenger myPass3 = new Passenger("Ms","Joan","Doyle");
+        assertEquals("Ms",myPass3.getTitle());
+    }
+    */
+
+    //cleaner way to test
+    @ParameterizedTest
+    @ValueSource(strings = {"Mr","Mrs","Ms"})
+    void titleTestSuccess2(String title){
+
+        Passenger myPass4 = new Passenger(title, "John", "O'Leary");
+    }
+
+
+    @Test
+    void titleTestFail(){
+        //Mr Mrs or Ms only
+        assertThrows(IllegalArgumentException.class ,()-> {new Passenger("Fr","Todd","Umptious");});
     }
 
 
